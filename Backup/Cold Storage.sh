6122f7b10b852cf24220fa7745b79e8a20151ea4
Abhinav <140040239+Abhi-cc@users.noreply.github.com>
2024-09-04 18:11:08 -0700

    Update Cold Storage.sh

--- Backup/Cold Storage.sh
+++ Backup/Cold Storage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Add Comments and DEST and SRC  , Redirect erros to the error life , EXit the code if the statmement is not true.
 Date=$(date +%m)
 Red='\033[35;1;4m'
 Yellow='\033[1;36m'
