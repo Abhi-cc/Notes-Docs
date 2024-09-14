072d162189afaa12fafb734ebfcddacb5bb5b077
abhinav <abhinavmalhotra997@pm.me>
2024-09-13 18:39:16 -0700

    Added Concept , Test the script again.

--- Backup/Cold Storage.sh
+++ Backup/Cold Storage.sh
@@ -1,11 +1,11 @@
-#Concept  #!!! Pending   
+#Concept  
+#Simple backupscript , Rsync will copy the source dir to destination forlder and then tar will compress it and rm will remove the unarchieved dir. U can send the archieved file to cloud or external harddrive or remote server
 ##
 
 #!/bin/bash
 
 #Error Handling & Error Files
 error_log_file=/var/log/backup_error.log
-trap 'echo "There is error - $(date +%f) : Check $error_log_file" ; exit 1' ERR
 
 # Source and Desitnation Here
 Source_Dir=<Your Source Directory here>
@@ -21,7 +21,7 @@ NC='\033[0m'
 
 # Copy the Entire directory and send it to the destination directory 
 # If you want to send to another machine u can edit it like this  rsync -Avazh  --progress  $Source_Dir <Server Ipaddress>:"<Destination on different machine>/$Date" 1>> /dev/null 2>>$error_log_file
-rsync -Avazh  --progress  $Source_Dir "$Destination_Dir/$Date" 1>> /dev/null 2>>$error_log_file
+rsync -Avazh  --progress  "$Source_Dir" "$Destination_Dir/$Date" 1>> /dev/null 2>>$error_log_file
 
 # After the sync is done it will compress it only works on local machine  # !! Pending add command in rsync command to compress the directory on remote
 # It will create a Month directory and copy there
@@ -30,13 +30,12 @@ then
    tar -cvzf "$Destination_Dir/$Date.tar.gz" "$Destination_Dir/$Date"
 else
    echo -e " ${Red} Try again, There is Problem ${NC} Check $error_log_file" 
-   exit 1
 fi
 
 # If the compress is success it will delete the unarchieved directory
 if [ $? -eq 0 ]
 then
-   rm -r <DEST>/$Date
+   rm -r "$Destination_Dir/$Date"
 else
    echo -e " ${Red} Try again, There is Problem ${NC}"
 fi
